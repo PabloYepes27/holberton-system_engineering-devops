97b8c94ac0893c956a59f1366194bbdedaf2d9e5
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-14 16:02:56 -0500

    Task 0 - style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
+"""Write a Python script that, using this REST API,
+ for a given employee ID, returns information about
+  his/her TODO list progress."""
 import json
 import requests
 from sys import argv
 
-EMPLOYEE_NAME = ""
-NUMBER_OF_DONE_TASKS = 0
-TOTAL_NUMBER_OF_TASKS = 0
-TASK_TITLE = []
 
-response_user = requests.get("https://jsonplaceholder.typicode.com/users")
-users = json.loads(response_user.text)
-for user in users:
-    if user['id'] == int(argv[1]):
-        EMPLOYEE_NAME = user['name']
-        break
+if __name__ == "__main__":
+    EMPLOYEE_NAME = ""
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
 
-response_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-todos = json.loads(response_todo.text)
-for todo in todos:
-    if todo['userId'] == int(argv[1]):
-        if todo['completed'] is True:
-            NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(todo['title'])
-        TOTAL_NUMBER_OF_TASKS += 1
+    response_user = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = json.loads(response_user.text)
+    for user in users:
+        if user['id'] == int(argv[1]):
+            EMPLOYEE_NAME = user['name']
+            break
 
-print("Employee {} is done with tasks({}/{}):".format(
-    EMPLOYEE_NAME,
-    NUMBER_OF_DONE_TASKS,
-    TOTAL_NUMBER_OF_TASKS))
+    response_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    todos = json.loads(response_todo.text)
+    for todo in todos:
+        if todo['userId'] == int(argv[1]):
+            if todo['completed'] is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo['title'])
+            TOTAL_NUMBER_OF_TASKS += 1
 
-for quote in TASK_TITLE:
-    print("\t {}".format(quote))
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME,
+        NUMBER_OF_DONE_TASKS,
+        TOTAL_NUMBER_OF_TASKS))
+
+    for quote in TASK_TITLE:
+        print("\t {}".format(quote))
