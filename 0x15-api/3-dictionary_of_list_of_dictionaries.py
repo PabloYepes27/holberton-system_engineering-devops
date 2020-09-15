95eb6a1e390a99ab2f674e4be0599bab18dcd864
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-14 20:09:21 -0500

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,13 +16,24 @@ if __name__ == "__main__":
         "https://jsonplaceholder.typicode.com/todos").json()
 
     for user in users:
-        data[user.get("id")] = []
-        for todo in todos:
-            aux_dict["username"] = user.get("username")
-            aux_dict["task"] = todo.get("title")
-            aux_dict["completed"] = todo.get("completed")
-            data[user.get("id")].append(aux_dict)
-            aux_dict = {}
+        data[user.get('id')] = []
+        aux_dict[user.get('id')] = user.get('username')
+
+    for todo in todos:
+        task_dict = {}
+        task_dict["task"] = todo.get('title')
+        task_dict["completed"] = todo.get("completed")
+        task_dict['username'] = aux_dict.get(todo.get('userId'))
+        data.get(todo.get('userId')).append(task_dict)
+
+    # for user in users:
+    #     data[user.get("id")] = []
+    #     for todo in todos:
+    #         aux_dict["username"] = user.get("username")
+    #         aux_dict["task"] = todo.get("title")
+    #         aux_dict["completed"] = todo.get("completed")
+    #         data[user.get("id")].append(aux_dict)
+    #         aux_dict = {}
 
     # create and open a file and fill with the info above
     with open("todo_all_employees.json", "w", encoding="utf-8") as jsonfile:
