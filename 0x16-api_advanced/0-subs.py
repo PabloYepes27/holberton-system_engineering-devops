f2df4e06a8f6ebed439e49ae562044ae02d6b853
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-17 13:24:42 -0500

    task 0 pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-# from sys import argv
+""" Write a function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers) for a
+given subreddit. If an invalid subreddit is given, the function
+should return 0. """
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Returns the number of subscribers (not active users, total
+    subscribers) for a given subreddit.
+    """
     base_url = 'https://www.reddit.com'
     headers = {'user-agent': 'fake_user_agent'}
     dict_response = requests.get(
